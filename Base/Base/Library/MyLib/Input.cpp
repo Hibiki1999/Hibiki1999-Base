40b8ff24e0a8445890ac015ceabbaae48eaa6fb6
パン ウィリアム <chihun9909@gmail.com>
2021-02-02 12:26:10 +0900

    Update Input for multiplayer

--- Base/Base/Library/MyLib/Input.cpp
+++ Base/Base/Library/MyLib/Input.cpp
@@ -1,40 +1,81 @@
 #include "Input.h"
 
-int Input::prevState{ 0 }; // 1フレーム前の状態
-int Input::currentState{ 0 }; // 現在の状態#include "Input.h"
+int Input::prevStates[]; // 1フレーム前の状態
+int Input::currentStates[]; // 現在の状態
+
+std::unordered_map<int, int> Input::padDic; // パッド番号からDXの定義への辞書
+
+void Input::InitPadDictionary()
+{
+	// 辞書配列で対応関係の辞書を作成しておく
+	padDic[(int)Pad::Key] = DX_INPUT_KEY;
+	padDic[(int)Pad::One] = DX_INPUT_PAD1;
+	padDic[(int)Pad::Two] = DX_INPUT_PAD2;
+	padDic[(int)Pad::Three] = DX_INPUT_PAD3;
+	padDic[(int)Pad::Four] = DX_INPUT_PAD4;
+}
 
 void Input::Init()
 {
-	prevState = 0;
-	currentState = 0;
+	InitPadDictionary();// パッドの辞書配列の初期化
+
+		// キー状態をゼロリセット
+	for (int i = 0; i < (int)Pad::NUM; i++)
+		prevStates[i] = currentStates[i] = 0;
 }
 
 // 最新の入力状況に更新する処理。
 // 毎フレームの最初に（ゲームの処理より先に）呼んでください。
 void Input::Update()
 {
-	prevState = currentState;
-
-	currentState = GetJoypadInputState(DX_INPUT_KEY_PAD1);
+	for (int i = 0; i < (int)Pad::NUM; i++)
+	{    // 現在の状態を一つ前の状態として保存してGetJoypad..
+		prevStates[i] = currentStates[i];
+		currentStates[i] = GetJoypadInputState(padDic[i]);
+	}
 }
 
 // ボタンが押されているか？
-bool Input::GetButton(int buttonId)
+bool Input::GetButton(Pad pad, int buttonId)
 {
+	if (pad == Pad::None) return false; // Noneなら判別不要
+	else if (pad == Pad::All) // All指定の時は全てのパッド
+	{    // GetButtonの中でGetButtonを呼ぶ【再起呼出し】テクニック
+		for (int i = 0; i < (int)Pad::NUM; i++)
+			if (GetButton((Pad)i, buttonId))
+				return true; //押されているPadを発見！
+		return false; // 一つも押されていなかった
+	}
 	// 今ボタンが押されているかどうかを返却
-	return (currentState & buttonId) != 0;
+	return (currentStates[(int)pad] & buttonId) != 0;
 }
 
 // ボタンが押された瞬間か？
-bool Input::GetButtonDown(int buttonId)
+bool Input::GetButtonDown(Pad pad, int buttonId)
 {
+	if (pad == Pad::None) return false; // Noneなら判別不要
+	else if (pad == Pad::All) // All指定の時は全てのパッド
+	{    // 再起呼出しテクニック
+		for (int i = 0; i < (int)Pad::NUM; i++)
+			if (GetButtonDown((Pad)i, buttonId))
+				return true; //押されているPadを発見！
+		return false; // 一つも押されていなかった
+	}
 	// 今は押されていて、かつ1フレーム前は押されていない場合はtrueを返却
-	return ((currentState & buttonId) & ~(prevState & buttonId)) != 0;
+	return ((currentStates[(int)pad] & buttonId) & ~(prevStates[(int)pad] & buttonId)) != 0;
 }
 
 // ボタンが離された瞬間か？
-bool Input::GetButtonUp(int buttonId)
+bool Input::GetButtonUp(Pad pad, int buttonId)
 {
+	if (pad == Pad::None) return false; // Noneなら判別不要
+	else if (pad == Pad::All) // All指定の時は全てのパッド
+	{    // 再起呼出しテクニック
+		for (int i = 0; i < (int)Pad::NUM; i++)
+			if (GetButtonUp((Pad)i, buttonId))
+				return true; //押されているPadを発見！
+		return false; // 一つも押されていなかった
+	}
 	// 1フレーム前は押されていて、かつ今は押されている場合はtrueを返却
-	return ((prevState & buttonId) & ~(currentState & buttonId)) != 0;
+	return ((prevStates[(int)pad] & buttonId) & ~(currentStates[(int)pad] & buttonId)) != 0;
 }
--- Base/Base/Library/MyLib/Input.h
+++ Base/Base/Library/MyLib/Input.h
@@ -2,13 +2,30 @@
 #define INPUT_H_
 
 #include "DxLib.h"
+#include <unordered_map>//辞書配列
+
+enum class Pad
+{
+    All = -2, // すべてのうちどれか1つでも押されたとき
+    None = -1,
+    Key = 0, // キーボード
+    One,
+    Two,
+    Three,
+    Four,
+    NUM,
+};
 
 // 入力クラス
 class Input
 {
 public:
-    static int prevState; // 1フレーム前の状態
-    static int currentState; // 現在の状態
+    static int prevStates[(int)Pad::NUM]; // 1フレーム前の状態
+    static int currentStates[(int)Pad::NUM]; // 現在の状態
+
+    static std::unordered_map<int, int> padDic;
+
+    static void InitPadDictionary();//辞書配列の初期化
 
     // 初期化。最初に1回だけ呼んでください。
     static void Init();
@@ -18,13 +35,13 @@ public:
     static void Update();
 
     // ボタンが押されているか？
-    static bool GetButton(int buttonId);
+    static bool GetButton(Pad pad, int buttonId);
 
     // ボタンが押された瞬間か？
-    static bool GetButtonDown(int buttonId);
+    static bool GetButtonDown(Pad pad, int buttonId);
 
     // ボタンが離された瞬間か？
-    static bool GetButtonUp(int buttonId);
+    static bool GetButtonUp(Pad pad, int buttonId);
 };
 
 #endif
