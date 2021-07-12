2270ff8dcee3c60b9f08f51bb60a1cc2decca50f
パン ウィリアム <chihun9909@gmail.com>
2021-07-12 23:44:33 +0900

    component

---
+++ Base/Base/Library/Player.cpp
@@ -0,0 +1,283 @@
+#include "Player.h"
+
+#include <DxLib.h>
+#include "../MyLib/Input.h"
+#include "../MyLib/MyMath.h"
+#include "../Map.h"
+#include "../Music.h"
+
+#include <cmath> 
+
+void Player::Init()
+{
+	AnimationAttach((int)State::wait);
+
+	// プレイヤーモデルの角度
+	modelAngle_ = -((camera.nowCamAngle_ - 90) * MyMath::Deg2Rad);
+
+	// カメラ位置
+	Vector3 cameraPos
+	{ /* X座標 */gm.player->position.x + (camDistanceFromPlayer_ * std::cos(camera.nowCamAngle_ * MyMath::Deg2Rad)),
+		/* Y座標 */ gm.player->position.y - camHeightFromTerrain_,
+		/* Z座標 */ gm.player->position.z + (camDistanceFromPlayer_ * std::sin(camera.nowCamAngle_ * MyMath::Deg2Rad)) };
+	camera.SetPosition(cameraPos);
+
+	//カメラはプレイヤの方を見る
+	Vector3 lookPos{ gm.player->position.x, gm.player->position.y + 100, gm.player->position.z };
+	camera.LookAt(lookPos);
+}
+
+void Player::HandleInput()
+{
+	if (!attachCheck_[(int)State::collect])
+	{
+		if (Input::GetButton(PAD_INPUT_4))              // A
+		{
+			moveSpeed_ += IncreaseSpeed_;
+			if (moveSpeed_ > moveSpeedMax_)
+				moveSpeed_ = moveSpeedMax_;      //Maxスピードで止める
+			if (!attachCheck_[(int)State::run] && !attachCheck_[(int)State::collect])
+				AnimationAttach((int)State::run);
+			modelAngle_ = -(camera.nowCamAngle_ * MyMath::Deg2Rad);
+		}
+		else if (Input::GetButton(PAD_INPUT_6))         // D
+		{
+			moveSpeed_ += IncreaseSpeed_;
+			if (moveSpeed_ < -moveSpeedMax_)
+				moveSpeed_ = -moveSpeedMax_;     //Maxスピードで止める
+			if (!attachCheck_[(int)State::run] && !attachCheck_[(int)State::collect])
+				AnimationAttach((int)State::run);
+			modelAngle_ = -((camera.nowCamAngle_ - 180) * MyMath::Deg2Rad);
+		}
+
+		if (Input::GetButton(PAD_INPUT_8))              // W
+		{
+			moveSpeed_ += IncreaseSpeed_;
+			if (moveSpeed_ > moveSpeedMax_)
+				moveSpeed_ = moveSpeedMax_;             //Maxスピードで止める
+			if (!attachCheck_[(int)State::run] && !attachCheck_[(int)State::collect])
+				AnimationAttach((int)State::run);
+			modelAngle_ = -((camera.nowCamAngle_ - 90) * MyMath::Deg2Rad);
+		}
+		else if (Input::GetButton(PAD_INPUT_5))         // S
+		{
+			moveSpeed_ += IncreaseSpeed_;
+			if (moveSpeed_ < -moveSpeedMax_)
+				moveSpeed_ = -moveSpeedMax_;           //Maxスピードで止める
+			if (!attachCheck_[(int)State::run] && !attachCheck_[(int)State::collect])
+				AnimationAttach((int)State::run);
+			modelAngle_ = -((camera.nowCamAngle_ + 90) * MyMath::Deg2Rad);
+		}
+
+		if (Input::GetButton(PAD_INPUT_8) && Input::GetButton(PAD_INPUT_4)) // 左前
+		{
+			modelAngle_ = -((camera.nowCamAngle_ - 45) * MyMath::Deg2Rad);
+		}
+		else if (Input::GetButton(PAD_INPUT_8) && Input::GetButton(PAD_INPUT_6)) // 右前
+		{
+			modelAngle_ = -((camera.nowCamAngle_ - 135) * MyMath::Deg2Rad);
+		}
+		else if (Input::GetButton(PAD_INPUT_5) && Input::GetButton(PAD_INPUT_4)) // 左後ろ
+		{
+			modelAngle_ = -((camera.nowCamAngle_ + 45) * MyMath::Deg2Rad);
+		}
+		else if (Input::GetButton(PAD_INPUT_5) && Input::GetButton(PAD_INPUT_6)) // 右後ろ
+		{
+			modelAngle_ = -((camera.nowCamAngle_ + 135) * MyMath::Deg2Rad);
+		}
+
+		if (!Input::GetButton(PAD_INPUT_4) &&
+			!Input::GetButton(PAD_INPUT_6) &&
+			!Input::GetButton(PAD_INPUT_5) &&
+			!Input::GetButton(PAD_INPUT_8))
+		{
+			if (!attachCheck_[(int)State::wait] && !attachCheck_[(int)State::collect])
+				AnimationAttach((int)State::wait);
+		}
+		else
+		{
+			if (tapSoundTimer_ >= TapSoundTime_)
+			{
+				PlaySoundMem(Music::run_SE, DX_PLAYTYPE_BACK);
+				tapSoundTimer_ = 0;
+			}
+			tapSoundTimer_++;
+		}
+	}
+}
+
+// 更新処理
+void Player::Update()
+{
+	Collider* com = test.GetComponent<Collider>();
+
+	if (com != nullptr)
+	{
+		//Component *baseCom = static_cast<Component*>(com); // base基底は呼ばれない
+		com->Update();
+		
+	}
+
+	camera.Move();
+	backX = camera.backX;
+
+	velocity = 0;
+
+	moveSpeed_        *= DecreaseSpeed_;                 // 移動速度も減速する
+
+	// 入力を受けての処理 
+	HandleInput();
+
+	if (modelAngle_ > 0)
+		modelAngle_ -= 360.0f * MyMath::Deg2Rad;         // プレイヤーの角度のずれを直す
+
+	float vx = (float)std::sin(modelAngle_) * moveSpeed_ * (modelAngle_ / std::abs(modelAngle_));
+	float vz = (float)std::cos(modelAngle_) * moveSpeed_ * (modelAngle_ / std::abs(modelAngle_));
+	velocity = { vx, 0.0f, vz };
+
+	if ((Input::GetButton(PAD_INPUT_8) && Input::GetButton(PAD_INPUT_4)) ||
+		(Input::GetButton(PAD_INPUT_8) && Input::GetButton(PAD_INPUT_6)) ||
+		(Input::GetButton(PAD_INPUT_5) && Input::GetButton(PAD_INPUT_4)) ||
+		(Input::GetButton(PAD_INPUT_5) && Input::GetButton(PAD_INPUT_6)))
+	{
+		velocity /= MyMath::Sqrt2;
+	}
+
+	// 実際に位置を動かす
+	position += velocity;
+
+	// カメラ位置
+	Vector3 cameraPos
+	{ /* X座標 */gm.player->position.x + (camDistanceFromPlayer_ * std::cos(camera.nowCamAngle_ * MyMath::Deg2Rad)),
+		/* Y座標 */ gm.player->position.y - camHeightFromTerrain_,
+		/* Z座標 */ gm.player->position.z + (camDistanceFromPlayer_ * std::sin(camera.nowCamAngle_ * MyMath::Deg2Rad)) };
+	camera.SetPosition(cameraPos);
+
+	//カメラはプレイヤの方を見る
+	Vector3 lookPos{ gm.player->position.x, gm.player->position.y + 100, gm.player->position.z };
+	camera.LookAt(lookPos);
+
+	if (attachCheck_[(int)State::collect])
+		PlayAnimation(0.5f, FALSE);
+	else if (attachCheck_[(int)State::run])
+		PlayAnimation(0.6f, TRUE);
+	else
+		PlayAnimation(0.07f, TRUE);
+
+	// マウス位置の初期化
+	SetMousePoint(Screen::width / 2, Screen::height / 2);
+}
+
+// 描画処理
+void Player::Draw()
+{
+	// プレイヤーモデルを回転させる
+	MV1SetRotationXYZ(gm.image.Player, VGet(0, modelAngle_, 0));
+	MV1SetPosition(gm.image.Player, position.Vec3ToVec());
+	MV1SetScale(gm.image.Player, VGet(0.8f, 0.8f, 0.8f));
+	MV1DrawModel(gm.image.Player);
+	//DrawHitBox(); 
+}
+
+
+void  Player::OnCollision(std::shared_ptr<GameObject> other)
+{
+	if (other->tag == "FieldItem")
+	{
+		if (Input::GetButtonDown(PAD_INPUT_1) && gm.pouch.size() < gm.MaxPouchSize)
+		{
+			if (!attachCheck_[(int)State::collect])
+				AnimationAttach((int)State::collect);
+		}
+		else if (Input::GetButtonDown(PAD_INPUT_1) && gm.pouch.size() >= gm.MaxPouchSize)
+		{
+			PlaySoundMem(Music::error_SE, DX_PLAYTYPE_BACK);
+		}
+	}
+
+	else if (other->tag != "Bush_1" && other->tag != "Bush_2"      && other->tag != "Bush_3" &&
+		     other->tag != "Bush_4" && other->tag != "BushLarge_1" && other->tag != "BushLarge_2")
+	{
+		if (
+			(GetBack() + 10.0f < other->GetForward() && GetForward() - 10.0f > other->GetBack()) &&
+			((GetLeft() - 1.0f < other->GetRight() || GetRight() + 1.0f > other->GetLeft()))
+			)
+		{
+			if (GetLeft() < other->GetRight() &&
+				GetLeft() > other->GetLeft())
+			{
+				SetLeft(other->GetRight() + 1.0f);
+				return;
+			}
+			else if (GetRight() > other->GetLeft() &&
+				GetRight() < other->GetRight())
+			{
+				SetRight(other->GetLeft() - 1.0f);
+				return;
+			}
+		}
+		else
+		{
+			if (GetBack() < other->GetForward() &&
+				GetBack() > other->GetBack())
+			{
+				SetBack(other->GetForward() + 1.0f);
+				return;
+			}
+			else if (GetForward() > other->GetBack() &&
+				GetForward() < other->GetForward())
+			{
+				SetForward(other->GetBack() - 1.0f);
+				return;
+			}
+		}
+	}
+}
+
+void Player::AnimationAttach(int AnimIndex)
+{
+	attachCheck_[prevAnimIndex_] = false;             // 前回立てたフラグの初期化
+
+	MV1DetachAnim(gm.image.Player, prevAttachIndex_); // アニメーションのデタッチ
+
+	prevAnimIndex_   = AnimIndex;                     // 次フラグを初期化するために保存
+	prevAttachIndex_ = attachIndex_;                  // 今のアニメを次デタッチするために保存
+
+	/*-----アタッチ処理-----*/
+	attachIndex_ = MV1AttachAnim(gm.image.Player, AnimIndex, -1, FALSE);
+	totalTime_   = MV1GetAttachAnimTotalTime(gm.image.Player, attachIndex_);
+	playTime_    = 0.0f;
+	MV1SetAttachAnimTime(gm.image.Player, attachIndex_, playTime_);
+
+	/*---------------*/
+
+	attachCheck_[AnimIndex] = true; // アニメーションによってフラグを立てる	
+}
+
+void Player::PlayAnimation(float ConstPlayTime, bool isLoop)
+{
+	if (isLoop)
+	{
+		playTime_ += ConstPlayTime;
+
+		if (playTime_ >= totalTime_)
+		{
+			playTime_ = 0.0f;
+		}
+		MV1SetAttachAnimTime(gm.image.Player, attachIndex_, playTime_);
+	}
+	else
+	{
+		playTime_ += ConstPlayTime;
+
+		if (playTime_ >= totalTime_)
+		{
+			for (int i = 0; i < attachCheck_.size(); i++)
+			{
+				attachCheck_[i] = false;
+			}
+		}
+
+		MV1SetAttachAnimTime(gm.image.Player, attachIndex_, playTime_);
+	}
+}
---
+++ Base/Base/Library/Player.h
@@ -0,0 +1,109 @@
+#ifndef PLAYER_H_
+#define PLAYER_H_
+
+
+#include "../GameManager.h"
+#include "GameObject.h"
+#include "../Screen.h"
+
+#include "../Component.h"
+
+enum class State
+{
+	collect,
+	run,
+	wait
+};
+
+class Player : public GameObject
+{
+public:
+	Component test;
+
+	GameManager& gm = GameManager::GetInstance();
+
+	Player() = default;
+	Player(Vector3 position)
+	{
+		this->test = Component(this);
+		this->AddComponent(&(this->test));
+
+		this->position = position;
+		tag = "Player";
+
+		// 画像ピクセル数
+		imageWidth          = 64;
+		imageHeight         = 64;
+		imageLength         = 64;
+		// 当たり判定の左端のオフセット
+		hitboxOffsetLeft    = 0;
+		hitboxOffsetRight   = 0;
+		hitboxOffsetTop     = 0;
+		hitboxOffsetBottom  = 0;
+		hitboxOffsetForward = 0;
+		hitboxOffsetBack    = 0;
+
+		if (isRotaGraph == true)
+		{   // 画像の中心を起点に描く場合のずれ
+			rotaGraphShift = { (float)imageWidth / 2, (float)imageHeight / 2, (float)imageLength / 2 };
+		}
+
+		// ミドルの値を初期化のときに計算して固定値を得る
+		collisionDetectionMiddleX_ = (GetRight() - GetLeft()) / 2;
+		collisionDetectionMiddleZ_ = (GetForward() - GetBack()) / 2;
+
+		SetMousePoint(Screen::width / 2, Screen::height / 2); // マウスカーソル初期化
+
+		camDistanceFromPlayer_ = 250; // プレイヤからカメラまでのY平面上の距離
+		camHeightFromTerrain_  = -200; // 地面からカメラまでのY方向上の高さ
+
+	};
+	~Player() = default;
+
+	void Init();
+	void Update() override;
+	void Draw() override;
+	void OnCollision(std::shared_ptr<GameObject> other);
+
+
+	int backX = 0;
+
+private:
+	void HandleInput();
+
+	void MoveX();
+	void MoveY();
+	void MoveZ();
+
+	void AnimationAttach(int AtnimIndex);
+	void PlayAnimation(float ConstPlayTime, bool isLoop);
+
+	int attachIndex_     = 0;
+	int prevAttachIndex_ = 0;
+	int prevAnimIndex_   = 0;
+	int tapSoundTimer_   = 0;
+
+	float totalTime_                  = 0.0f;
+	float playTime_                   = 0.0f;
+	float modelAngle_                 = 0.0f;
+	float moveSpeedMax_               = 15.0f;
+	float moveSpeed_                  = 0.0f;
+	float camDistanceFromPlayer_      = 0.0f;
+	float camHeightFromTerrain_       = 0.0f;
+	float collisionDetectionMiddleX_  = 0.0f;
+	float collisionDetectionMiddleZ_  = 0.0f;
+
+	bool collectAttach_          = false;
+	bool runAttach_              = false;
+	bool waitAttach_             = false;
+	std::vector<bool> attachCheck_{ collectAttach_, runAttach_, waitAttach_ };
+
+	const int DrawStrPosX_  = 500;
+	const int DrawStrPosY_  = 500;
+	const int TapSoundTime_ = 30;
+
+	const float IncreaseSpeed_ = 0.5f;
+	const float DecreaseSpeed_ = 0.8f;
+};
+
+#endif
